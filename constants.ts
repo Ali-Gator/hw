d2f705dadf99cb4841f2c35f1f5dfcb492437ec6
Oleg Gordienov <oleg.gordienov@ringcentral.com>
2024-10-18 16:48:52 +0300

    Add first part

---
+++ constants.ts
@@ -0,0 +1,1 @@
+export const mnemonic = "craft edge frog guide music slide often awesome stove judge cargo orbit spread brain lady memory pride sentence trend wing luggage blame artwork differ"; // your 24 secret words (replace ... with the rest of the words)
