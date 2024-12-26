e94e3dee83abd8e3548fd85cf80421a117808040
Oleg G <ogordienov@yandex.ru>
2024-12-26 21:19:28 +0200

    update git ignore

--- part3/src/hooks/useCounterContract.ts
+++ part3/src/hooks/useCounterContract.ts
@@ -2,12 +2,16 @@ import { useEffect, useState } from 'react';
 import Counter from '../contracts/Counter';
 import { useTonClient } from './useTonClient';
 import { useAsyncInitialize } from './useAsyncInitialize';
+import { useTonConnect } from './useTonConnect';
 import { Address, OpenedContract } from '@ton/core';
 import { contractCouterAddress } from '../../constants.ts';
 
 export function useCounterContract() {
   const client = useTonClient();
-  const [val, setVal] = useState<null | number>(null);
+  const [val, setVal] = useState<null | string>();
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
 
   const counterContract = useAsyncInitialize(async () => {
     if (!client) return;
@@ -22,7 +26,9 @@ export function useCounterContract() {
       if (!counterContract) return;
       setVal(null);
       const val = await counterContract.getCounter();
-      setVal(Number(val));
+      setVal(val.toString());
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
     }
     getValue();
   }, [counterContract]);
@@ -30,5 +36,8 @@ export function useCounterContract() {
   return {
     value: val,
     address: counterContract?.address.toString(),
+    sendIncrement: () => {
+      return counterContract?.sendIncrement(sender);
+    },
   };
 }
